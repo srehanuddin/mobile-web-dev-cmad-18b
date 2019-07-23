1ffe56dde534a8e9b0deea19e7874a936e10a9ba
Muhammad Tabish <tabish615@gmail.com>
2019-07-24 00:41:07 +0500

    class 2019-07-21

---
+++ React Apps/first-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<h2>Hello World!</h2>, document.getElementById('root'));
+
+
